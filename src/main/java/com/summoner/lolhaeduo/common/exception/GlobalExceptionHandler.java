06e6e9ac4bee3bf01230bf59a75c05ad612170d1
hubaek <zjfl3122@naver.com>
2025-04-05 23:44:35 +0900

    Feat : GlobalExceptionHandler 추가

---
+++ src/main/java/com/summoner/lolhaeduo/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.summoner.lolhaeduo.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    public ResponseEntity<Map<String, Object>> handleRateLimitExceeded(RateLimitExceededException ex) {
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(Map.of(
+                "message", ex.getMessage(),
+                "code", "RATE_LIMIT_EXCEEDED",
+                "timestamp", Instant.now().toString()
+        ));
+    }
+
+}
