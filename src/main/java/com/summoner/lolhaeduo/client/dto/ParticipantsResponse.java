f8e1d54a03f47a0d1de86b7a75e3a457c60f2eb7
hubaek <zjfl3122@naver.com>
2025-04-16 20:48:23 +0900

    Refactor : ParticipantsResponse 필드 변경
    - 기존 riotIdGameName와 riotIdTagline로 해당 매치의 참가자를 판단 -> 띄어쓰기를 정확히 안하면 인식을 못함
    - puuid로 구분하는 것으로 변경

--- src/main/java/com/summoner/lolhaeduo/client/dto/ParticipantsResponse.java
+++ src/main/java/com/summoner/lolhaeduo/client/dto/ParticipantsResponse.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class ParticipantsResponse {
-    private final String riotIdGameName;
-    private final String riotIdTagline;
+    private final String puuid;
     private final int assists;
     private final String championName;
     private final int deaths;
