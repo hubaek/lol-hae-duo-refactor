281cd0f7aa34a8fba0198deb71b80fcc2d23cb83
hubaek <zjfl3122@naver.com>
2025-05-21 02:27:36 +0900

    feat : 스케줄 어노테이션 사용을 위한 기능 추가

--- src/main/java/com/summoner/lolhaeduo/LolHaeDuoApplication.java
+++ src/main/java/com/summoner/lolhaeduo/LolHaeDuoApplication.java
@@ -3,13 +3,17 @@ package com.summoner.lolhaeduo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
 public class LolHaeDuoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LolHaeDuoApplication.class, args);
+		SpringApplication app = new SpringApplication(LolHaeDuoApplication.class);
+		app.setAdditionalProfiles("redis");
+		app.run(args);
 	}
 
 }
