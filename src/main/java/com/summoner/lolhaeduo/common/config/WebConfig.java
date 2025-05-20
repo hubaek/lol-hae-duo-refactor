eca4caa88e50cca7404fbb15448bec3478eade5a
hubaek <zjfl3122@naver.com>
2025-05-20 23:47:00 +0900

    feat : WebConfig ObjectMapper 추가

--- src/main/java/com/summoner/lolhaeduo/common/config/WebConfig.java
+++ src/main/java/com/summoner/lolhaeduo/common/config/WebConfig.java
@@ -1,7 +1,12 @@
 package com.summoner.lolhaeduo.common.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +21,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
+    }
+
+    @Bean(name = "webObjectMapper")
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Java 8 날짜/시간 모듈 등록
+        objectMapper.registerModule(new JavaTimeModule());
+
+        // HTTP 요청/응답에 필요한 설정 추가
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter(objectMapper());
     }
 }
