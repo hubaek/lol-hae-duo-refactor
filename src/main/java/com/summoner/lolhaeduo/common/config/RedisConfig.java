f431dbb8c0989d8a70b4da848d5ac208d0ed1c2c
hubaek <zjfl3122@naver.com>
2025-05-21 00:27:47 +0900

    refactor : RedisConfig 개선
    - Redis 관련 설정에 집중
    - 타입 정보가 필요한 ObjectMapper를 주입받아 사용
    - 내부에서 새로운 ObjectMapper를 생성하지 않고 기존 빈 활용

--- src/main/java/com/summoner/lolhaeduo/common/config/RedisConfig.java
+++ src/main/java/com/summoner/lolhaeduo/common/config/RedisConfig.java
@@ -1,8 +1,13 @@
 package com.summoner.lolhaeduo.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +16,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -24,21 +30,39 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    /**
+     * Redis 연결 팩토리 설정
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
         return new LettuceConnectionFactory(redisConfig);
     }
 
+    /**
+     * RedisTemplate 설정
+     * 타입 정보가 포함된 ObjectMapper를 사용하여 Redis에 저장할 때 JSON으로 직렬화
+     */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory,
+            @Qualifier("typedObjectMapper") ObjectMapper typedObjectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // 키는 문자열로, 값은 JSON으로 직렬화
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(typedObjectMapper));
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(typedObjectMapper));
+
         return template;
     }
 
+    /**
+     * RedissonClient 설정
+     * 분산 락, 퍼블리셔/서브스크라이버 등 고급 기능 사용 시 활용
+     */
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
