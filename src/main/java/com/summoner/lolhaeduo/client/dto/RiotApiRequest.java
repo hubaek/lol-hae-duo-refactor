8064a2a98475c3db50ae53fcac443b8fc2e6acf8
hubaek <zjfl3122@naver.com>
2025-05-21 01:36:46 +0900

    refactor : RiotApiRequest 개선
    - Jackson 객체 역직렬화로 인해 기본생성자, 빌더패턴으로 인해 AllArgsConstructor 추가

--- src/main/java/com/summoner/lolhaeduo/client/dto/RiotApiRequest.java
+++ src/main/java/com/summoner/lolhaeduo/client/dto/RiotApiRequest.java
@@ -1,22 +1,31 @@
 package com.summoner.lolhaeduo.client.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @Data
+@NoArgsConstructor // Jackson이 객체를 역직렬화 할때 기본생성자 필요
+@AllArgsConstructor
 @Builder
-public class RiotApiRequest<T> {
+public class RiotApiRequest<T> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private String requestId;
     private RequestType requestType;
     private Map<String, Object> parameters;
     private int retryCount;
     private LocalDateTime createdAt;
     private LocalDateTime nextAttemptTime;
-    private CompletableFuture<T> resultFuture;
+    private transient CompletableFuture<T> resultFuture;
 
     public enum RequestType {
         EXTRACT_PUUID,
